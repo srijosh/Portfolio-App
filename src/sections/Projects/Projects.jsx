74c33f5254c4c8f9b8598fa583377a14ca0e20d7
Srijan Joshi <srijanjoshi7@gmail.com>
2026-07-15 11:07:43 +0545

    projects update

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -1,8 +1,6 @@
 import styles from './ProjectsStyles.module.css';
-// import face from '../../assets/face.png';
-// import website from '../../assets/website.png';
+import roam from '../../assets/roam.png';
 import naruto from '../../assets/naruto.png';
-import medical from '../../assets/medical.png';
 import ProjectCard from '../../common/ProjectCard';
 import coffee from '../../assets/coffee.png';
 import news from '../../assets/news.png';
@@ -12,23 +10,11 @@ function Projects() {
     <section id="projects" className={styles.container}>
       <h1 className="sectionTitle">Projects</h1>
       <div className={styles.projectsContainer}>
-        {/* <ProjectCard
-          src={face}
-          link="https://github.com/srijosh/Face-Verification-Deep-Learning-"
-          h3="Face Verification"
-        /> */}
         <ProjectCard
           src={coffee}
           link="https://github.com/srijosh/Coffee-AI-Chatbot"
           h3="Coffee AI Chatbot"  
         />
-        
-        
-        {/* <ProjectCard
-          src={website}
-          link="https://github.com/srijosh/Website-RAG-Chat.git"
-          h3="Website Based RAG Chat"
-        /> */}
         <ProjectCard
           src={news}
           link="https://github.com/srijosh/News-Category-Classifier-Web-Scraping-and-LLM-Fine-Tuning"
@@ -40,9 +26,9 @@ function Projects() {
           h3="Naruto NLP Project"
         />
         <ProjectCard
-          src={medical}
-          link="https://github.com/srijosh/Medical-Chatbot-Vision-and-Voice.git"
-          h3="Medical Chatbot Vision and Voice"
+          src={roam}
+          link="https://github.com/srijosh/RoamAI-Multi-Agent-Travel-Planner-with-LangGraph.git"
+          h3="RoamAI Multi-Agent Travel Planner"
         />
       </div>
     </section>
